718c285fa7b1ccfe9e27d55b6f0792a376400df6
Diego Steiner <diego.steiner@u041.ch>
2020-11-18 16:12:15 +0100

    Fix ci

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import styles from './Layout.module.css'
-import React, { FunctionComponent, ReactChildren, ReactElement } from 'react'
+import React, { FunctionComponent } from 'react'
 
 export const Container: FunctionComponent<{}> = ({ children }) => {
   return (<div className={styles.container}>{children}</div>)
