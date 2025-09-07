c9be88b99132053766587a1072f7eed52f395279
Diego Steiner <diego.steiner@u041.ch>
2025-09-07 18:24:01 +0000

    fix: zustand

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
+import { createJSONStorage, persist } from "zustand/middleware";
 import { Course } from "./models/course";
 import { ImportTupel, transform } from "./models/import_data";
 
@@ -53,7 +53,7 @@ export const useStore = create<AppState>()(
     }),
     {
       name: "bsv-pdf-storage",
-      getStorage: () => sessionStorage,
+      storage: createJSONStorage(() => sessionStorage),
     }
   )
 );
