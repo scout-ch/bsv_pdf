fa727723c968aa440957016f575acdac2e63621a
Diego Steiner <diego.steiner@u041.ch>
2026-02-02 19:15:32 +0000

    chore: upgrade tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import react from "@vitejs/plugin-react";
-// import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
-// import { viteSingleFile } from "vite-plugin-singlefile"
+import tailwindcss from "@tailwindcss/vite";
 
 // const pwaOptions: Partial<VitePWAOptions> = {
 //   includeAssets: [
@@ -39,8 +38,7 @@ import react from "@vitejs/plugin-react";
 //   },
 // }
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "/bsv_pdf", // https://vitejs.dev/guide/static-deploy
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [react(), TanStackRouterVite(), tailwindcss()],
 });
